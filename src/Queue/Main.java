aac254a0b376388f60ad38dfa349fd8ba3503ec8
TeckWIT <114758496+TeckZin@users.noreply.github.com>
2023-11-04 00:41:16 -0400

    push all

--- src/Queue/Main.java
+++ src/Queue/Main.java
@@ -3,7 +3,11 @@ package Queue;
 public class Main <T>  {
 
     public static void main(String[] args) {
+        Store store = new Store(1.0);
 
+        while(true){
+            store.update();
+        }
     }
 
 }
