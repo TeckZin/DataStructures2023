7aa35896631996f24e149f418d62fbbab622eab3
TeckWIT <114758496+TeckZin@users.noreply.github.com>
2023-11-30 18:40:35 -0500

    push all

---
+++ src/Dictionary/DIctionaryInterface.java
@@ -0,0 +1,4 @@
+package Dictionary;
+
+public interface DIctionaryInterface {
+}
---
+++ src/Dictionary/Main.java
@@ -0,0 +1,10 @@
+package Dictionary;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+
+
+    }
+}
